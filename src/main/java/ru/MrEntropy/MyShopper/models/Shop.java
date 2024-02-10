305a41f177b8440a8ad8345bf33e856d9c3277d8
MrEnthropia <777ukv@gmail.com>
2024-02-10 13:21:20 +0300

    Создание репозитория проекта MyShopper Spring MVC version

---
+++ src/main/java/ru/MrEntropy/MyShopper/models/Shop.java
@@ -0,0 +1,79 @@
+package ru.MrEntropy.MyShopper.models;
+
+
+public class Shop {
+
+    public Shop(){
+
+    }
+    private int id;
+    private String name;
+    private String type;
+    private String category;
+    private String address;
+    private String website;
+    private String notes;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+
+
+
+
+}
